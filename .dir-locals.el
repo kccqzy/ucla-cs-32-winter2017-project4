9b711ade8e2699e8a3d92932d8822c55ebf9f54b
Zhouyu Qian <qzy@qzy.io>
2017-03-08 11:34:16 -0800

    Add metadata files

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-clang-args . "-Wno-deprecated-declarations")
+  (flycheck-clang-language-standard . "c++14")
+  (c-file-style . "linux")
+  (c-basic-offset . 4)))
