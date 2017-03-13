a9ee8ef0e742e342f241ba382c5481e675affb62
Zhouyu Qian <qzy@qzy.io>
2017-03-13 11:30:25 -0700

    Pairing heaps are probably not necessary

--- Navigator.cpp
+++ Navigator.cpp
@@ -17,77 +17,6 @@ bool operator==(GeoCoord const& a, GeoCoord const& b) {
     return std::make_pair(a.latitude, a.longitude) == std::make_pair(b.latitude, b.longitude);
 }
 
-namespace {
-template<typename T, typename Comp>
-class PairingHeap {
-private:
-    struct Node {
-        T d;
-        Node *youngestChild, *olderSibling, *parentOrYoungerSibling;
-    };
-    Comp comp;
-    Node* root;
-    Node* link(Node* a, Node* b) const {
-        if (!a && !b) return nullptr;
-        if (!a) return b;
-        if (!b) return a;
-        assert(!a->olderSibling);
-        assert(!b->olderSibling);
-        if (comp(b->d, a->d)) std::swap(a, b);
-        if (a->youngestChild) {
-            b->olderSibling = a->youngestChild;
-            a->youngestChild = b;
-        } else {
-            a->youngestChild = b;
-        }
-        b->parentOrYoungerSibling = a;
-        a->parentOrYoungerSibling = nullptr;
-        return a;
-    }
-    static Node* unlink(Node* node) {
-        assert(node);
-        assert(node->parentOrYoungerSibling);
-        if (node == node->parentOrYoungerSibling->youngestChild)
-            node->parentOrYoungerSibling->youngestChild = node->olderSibling;
-        else
-            node->parentOrYoungerSibling->olderSibling = node->olderSibling;
-        node->parentOrYoungerSibling = nullptr;
-        node->olderSibling = nullptr;
-        return node;
-    }
-
-public:
-    typedef Node* HeapItem;
-    PairingHeap() : comp{}, root{nullptr} {}
-    Node* findMin() const { return root; }
-    Node* insert(T const& d) {
-        root = link(root, new Node{d, nullptr, nullptr, nullptr});
-        return !comp(root->d, d) && !comp(d, root->d) ? root : root->youngestChild;
-    }
-    void modify(Node* node, T const& newD) {
-        node->d = newD;
-        if (node != root) {
-            unlink(node);
-            root = link(root, node);
-        }
-    }
-    void deleteMin() {
-        Node* oldRoot = root;
-        std::vector<Node*> allChildren;
-        for (Node* i = root->youngestChild; i; i = i->olderSibling) allChildren.push_back(i);
-        for (Node* i : allChildren) {
-            i->olderSibling = nullptr;
-            i->parentOrYoungerSibling = nullptr;
-        }
-        for (size_t i = 0; i < (allChildren.size() & (~1ull)); i += 2)
-            allChildren[i] = link(allChildren[i], allChildren[i | 1]);
-        root = (allChildren.size() & 1) ? allChildren.back() : nullptr;
-        for (size_t i = (allChildren.size() & (~1ull)); i; i -= 2) root = link(root, allChildren[i - 2]);
-        delete oldRoot;
-    }
-};
-}
-
 class NavigatorImpl {
 private:
     SegmentMapper segmentMapper;
@@ -200,13 +129,18 @@ public:
         while (!nodeRanks.empty()) {
             auto currentIt = nodeRanks.top().it;
             nodeRanks.pop();
-            if (currentIt->second.optimisticEstimate == HUGE_VAL) continue;
+            if (currentIt->second.optimisticEstimate == HUGE_VAL) {
+                // The priority_queue might contain duplicates, and the later
+                // ones might have already been evaluated.
+                continue;
+            }
             if (isGeoCoordOnSegment(currentIt->first, endStreetSegment)) {
                 reconstructPath(startCoord, endCoord, startStreetSegment, currentIt->first, discoveredNodes,
                                 directions);
                 return NAV_SUCCESS;
             }
 
+            // Use this as a marker for completion of evaluation of a node.
             currentIt->second.optimisticEstimate = HUGE_VAL;
             for (auto const& neighbor : getNeighbors(currentIt->first)) {
                 double distance = currentIt->second.travelledDistance + distanceEarthKM(currentIt->first, neighbor);
@@ -214,13 +148,15 @@ public:
                 double estimate = distance + distanceEarthKM(neighbor, endCoord);
                 assert(estimate > 0);
                 auto it = discoveredNodes.find(neighbor);
-                if (it == discoveredNodes.end())
+                if (it == discoveredNodes.end()) {
                     nodeRanks.emplace(
                       estimate,
                       discoveredNodes.emplace(neighbor, DiscoveredNode(currentIt->first, distance, estimate)).first);
-                else if (it->second.optimisticEstimate == HUGE_VAL || distance >= it->second.travelledDistance)
+                } else if (it->second.optimisticEstimate == HUGE_VAL || distance >= it->second.travelledDistance) {
                     continue;
-                else {
+                } else {
+                    // Inserting duplicate. Statistics have shown that in
+                    // practice only 8% of inserts are duplicates.
                     it->second = DiscoveredNode(currentIt->first, distance, estimate);
                     nodeRanks.emplace(estimate, it);
                 }
